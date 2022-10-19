6e119e988d0e26cc1b8119f299bfb5189e168e81
Victor Nunez <VN135766@gmail.com>
2022-10-19 13:29:57 -0400

    fixed bug that would prevent data from updating after first search

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 var userInputCity
-var currentWeatherData
+
 
 var weatherData = []
 $('#submitCityInput').on('click', function(event) {
   event.preventDefault()
-  console.log("click!");
+
   resetData();
+  console.log(userInputCity)
   userInputCity = $('#cityInput').val();
   console.log(userInputCity)
   addPreviousSearchButton();
@@ -25,7 +26,8 @@ $('#previous-search').on('click', '#delete-button' ,function(event) {
 }
 function resetData() {
   $('section').empty();
-  userInputCity=""
+  userInputCity="";
+  weatherData= []
   currentWeatherData=""
 }
 
